7faa3a9230c7bf42b2d1e224ea9f9aa50871eafc
Live session user <danzy01@interia.pl>
2022-08-08 08:08:20 +0000

    Rozdzielenie pliku funkcyjnego na plik z funckcjami watkow oraz osobny plik na funkcje nie zwiazane z watkami :) Lekkie modyfikacje wyswietlania wynikow na konsoli

--- func.c
+++ func.c
@@ -30,23 +30,6 @@ void free_global(void){
     
 }
 
-void *thread_func(void *arg){
-    size_t i=0,j=0; 
-    sleep(1);   
-    pthread_mutex_lock(&mutex);
-            
-     for(i=0;i<a;i++){
-        for(j=0;j<b;j++){
-            printf("%ld",tab[i][j]);            
-        }    
-        printf("\n");    
-    }    
-    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
-
-
-
 void size_check(void){
     char buf[buf_size];    
     FILE *fp;
@@ -61,80 +44,4 @@ void size_check(void){
     core_amount = size;
     fclose(fp);
 }
-void *read_th(void *arg){
-    size_t i=0,j=0;
-    char buf[buf_size];
-    char* token;     
-    FILE *fp; 
-    sleep(1); 
-    pthread_mutex_lock(&mutex);
-        
-    fp=fopen("/proc/stat","r");
-    i=0;    
-    while(fgets(buf,buf_size,fp) && (i<4)){
-        if(strstr(buf,"intr")!=NULL)break;    
-              
-        token=strtok(buf," ");
-        j=0   ;     
-        while(token){
-            if(j==0)tab[i][j]=0;
-            else{          
-            tab[i][j]=atol(token);
-            }           
-            token=strtok(NULL," ");
-          j++;            
-        }       
-        memset(&buf[0],0,sizeof(buf));
-        i++;    
-    }
-    fclose(fp);
-    pthread_mutex_unlock(&mutex);
-    
-    return NULL;
-}
 
-void *analyze_th(void *arg){
-    size_t i=0,j=0; 
-    size_t Idle,PIdle,PNonIdle,PTotal,NonIdle,Total,totald,idled;   
-    pthread_mutex_lock(&mutex);
-        
-    for(i=0;i<core_amount;i++){
-        Idle = tab[i][4]+tab[i][5];
-        PIdle =ptab[i][4]+ptab[i][5];
-        
-        NonIdle = tab[i][1]+tab[i][2]+tab[i][3]+tab[i][6]+tab[i][7]+tab[i][8];
-PNonIdle = ptab[i][1]+ptab[i][2]+ptab[i][3]+ptab[i][6]+ptab[i][7]+ptab[i][8];    
-        
-        Total = Idle + NonIdle;
-        PTotal = PIdle + PNonIdle;
-        
-        totald = Total - PTotal;
-        idled = Idle - PIdle;
-        p_core_usage[i] = (double)(totald - idled)/(double)totald*100;
-        
-    }
-    for(i=0;i<core_amount;i++){
-            for(j=0;j<stat_fields;j++){
-                ptab[i][j] = tab[i][j];        
-            }    
-        }
-    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
-
-void *print_th(void *arg){
-    size_t i=0;   
-    pthread_mutex_lock(&mutex);
-    for(i=0;i<core_amount;i++){
-        if(i==0){        
-            printf("Total: %lf",p_core_usage[i]);
-            fputs("%\n",stdout);    
-        }
-        else{
-            printf("cpu%ld: %lf",i-1,p_core_usage[i]);
-            fputs("%\n",stdout);
-        }
-    }
-    pthread_mutex_unlock(&mutex);
-    return NULL;
-}
--- mainn.c
+++ mainn.c
@@ -1,12 +1,13 @@
 #include "header.h"
 #include "func.c"
+#include "thread_func.c"
 #include "unit_test.c"
 
 int main(){
         
     pthread_t reader,analyzer,printer;    
     size_check();
-    size_t i=0;
+    size_t i=0,from_second=0;
     
          
      
@@ -14,13 +15,21 @@ int main(){
         
             
       for(i=0;i<5;){              
+        if(from_second==1)fputs("Starting process monitoring\n",stdout);        
         pthread_create(&reader,NULL,&read_th,NULL);
         pthread_create(&analyzer,NULL,&analyze_th,NULL);
-        pthread_create(&printer,NULL,&print_th,NULL);        
+        if(from_second==2)pthread_create(&printer,NULL,&print_th,NULL);        
         pthread_join(reader,NULL);
         pthread_join(analyzer,NULL);
-        pthread_join(printer,NULL);
-        
+        if(from_second==2)pthread_join(printer,NULL);
+                
+        if(from_second==2){
+             unit_result_test();
+             printf("\n");       
+        }
+        else{
+        from_second++;        
+        }
         
               
         }
